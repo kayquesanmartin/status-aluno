4888bb354f3ddcc47903b3ad3ecd76aa6cba01ba
kayquesanmartin <kayque.sanmartin65@gmail.com>
2025-04-02 12:58:56 -0300

    feat: added Spinner for student selection and layout adjustments

--- app/src/main/java/io/github/kayquesanmartin/status_alunos/MainActivity2.kt
+++ app/src/main/java/io/github/kayquesanmartin/status_alunos/MainActivity2.kt
@@ -1,20 +1,48 @@
 package io.github.kayquesanmartin.status_alunos
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var alunoSpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        alunoSpinner = findViewById(R.id.alunoSpinner)
+
+        val options = resources.getStringArray(R.array.alunos)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, options)
+        alunoSpinner.adapter = adapter
+
+        alunoSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedAluno = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity2, "Aluno selecionado: $selectedAluno", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Não mudar nada")
+            }
+
         }
+
     }
 }
